098eea05aaac31845b598762f9f17fd803338cda
rocketpy <dimitr.ushakov@gmail.com>
2020-11-18 21:52:50 +0200

    updated

--- simple_configur_pand_prof.py
+++ simple_configur_pand_prof.py
@@ -1,1 +1,10 @@
-
+report = pandas_profiling.ProfileReport(df,
+                                        check_recoded=False,
+                                        check_correlation_pearson=False,
+                                        check_correlation_cramers=False,
+                                        missing_diagrams={'bar': False,
+                                                          'matrix': False,
+                                                          'heatmap': False,
+                                                          'dendrogram': False
+                                                           }
+                                        )
