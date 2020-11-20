3d6de36e006fc067b10caadb17b8ab17214b24b3
rocketpy <dimitr.ushakov@gmail.com>
2020-11-20 22:26:44 +0200

    updated

--- profiling_with_pandas.py
+++ profiling_with_pandas.py
@@ -3,6 +3,8 @@
 # Seaborn Datasets : ['anscombe', 'attention', 'brain_networks', 'car_crashes', 'diamonds', 'dots',
 # 'exercise', 'flights', 'fmri', 'gammas', 'iris', 'mpg', 'planets', 'tips', 'titanic']
 
+# pip install pandas-profiling
+
 import pandas as pd
 import seaborn as sns
 import pandas_profiling
@@ -16,3 +18,10 @@ df = sns.load_dataset('car_crashes')
 # some info about df
 # df.describe()
 # df.info()
+
+profile = pandas_profiling.ProfileReport(df)
+profile.to_file("Car_crashes.html")  # outputfile="Car_crashes.html"
+#  or
+df = pd.read_csv('file_name.csv')
+profile = pandas_profiling.ProfileReport(df)
+profile.to_file(outputfile="file_name.html")
