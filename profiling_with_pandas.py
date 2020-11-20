d567c5664c01eef151b8216a721259aceec4b66f
rocketpy <dimitr.ushakov@gmail.com>
2020-11-20 22:16:05 +0200

    updated

--- profiling_with_pandas.py
+++ profiling_with_pandas.py
@@ -1,1 +1,8 @@
+#  https://pandas-profiling.github.io/pandas-profiling/docs/
 
+# Seaborn Datasets : ['anscombe', 'attention', 'brain_networks', 'car_crashes', 'diamonds', 'dots',
+# 'exercise', 'flights', 'fmri', 'gammas', 'iris', 'mpg', 'planets', 'tips', 'titanic']
+
+import pandas as pd
+import seaborn as sns
+import pandas_profiling
