d1db46be80ce40f276152fa1bbe943a78d5e3ae1
rocketpy <dimitr.ushakov@gmail.com>
2020-11-20 22:09:16 +0200

    updated

--- merge_2_csv_to_1.py
+++ merge_2_csv_to_1.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+
+first = pd.DataFrame.from_csv("https://.csv")
+second = pd.DataFrame.from_csv("https://.csv")
+
+result = pd.merge(first, 
+                  second, 
+                  how='inner', 
+                  left_on="second_id", 
+                  right_on="id", 
+                  sort=True,
+                  suffixes=('_first', '_second'))
