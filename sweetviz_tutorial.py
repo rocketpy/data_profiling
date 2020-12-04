f646e1a7e87c33e378e81301bf55cf3331a32526
rocketpy <dimitr.ushakov@gmail.com>
2020-12-04 22:26:19 +0200

    updated

--- sweetviz_tutorial.py
+++ sweetviz_tutorial.py
@@ -1,1 +1,13 @@
+#  A pandas-based library to visualize and compare datasets !
 
+# PyPi: https://pypi.org/project/sweetviz/
+# GIT: https://github.com/fbdesignpro/sweetviz
+
+# pip install sweetviz
+
+
+import sweetviz as sv
+
+
+my_report = sv.analyze(my_dataframe)
+my_report.show_html() # Default arguments will generate to "SWEETVIZ_REPORT.html"
