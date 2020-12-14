19cc5af908cb9df9e6a929d9b57a784bdccb08b1
rocketpy <dimitr.ushakov@gmail.com>
2020-12-14 21:10:13 +0200

    updated

--- benchmark_compare.py
+++ benchmark_compare.py
@@ -7,8 +7,12 @@
 
 #  python -m pip install simple_benchmark
 
+#  Or installing the most recent version directly from git:
+#  python -m pip install git+https://github.com/MSeifert04/simple_benchmark.git
+
 #  example
 import numpy as np
+mport matplotlib.pyplot as plt
 from simple_benchmark import benchmark
 
 
@@ -17,3 +21,8 @@ arguments = {i: [1]*i for i in [1, 10, 100, 1000, 10000, 100000]}
 argument_name = 'list size'
 aliases = {sum: 'Python sum', np.sum: 'NumPy sum'}
 b = benchmark(funcs, arguments, argument_name, function_aliases=aliases)
+
+b.plot()  # matplotlib (has to be installed too)
+
+#  to save the plotted benchmark as PNG file
+plt.savefig('file_name.png')
