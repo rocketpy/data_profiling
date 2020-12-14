8fc550f065dfd4dcc80c85fa68452cbd6f44ba4a
rocketpy <dimitr.ushakov@gmail.com>
2020-12-14 21:05:44 +0200

    updated

--- benchmark_compare.py
+++ benchmark_compare.py
@@ -1,1 +1,9 @@
+#  Offical:  https://github.com/MSeifert04/simple_benchmark
+
+# The goal of this package is to provide a simple way to compare
+# the performance of different approaches for different inputs and to visualize the result.
+
+#  Dependencies:  NumPy, pandas, matplotlib
+
+#  python -m pip install simple_benchmark
 
