6a5c931a9d21ec2ccc6af771f84053c3689cb818
rocketpy <dimitr.ushakov@gmail.com>
2021-05-10 23:31:19 +0300

    Update Orange_tutor.py

--- Orange_tutor.py
+++ Orange_tutor.py
@@ -1,1 +1,15 @@
+#  Orange, a component-based data mining framework.
 
+# Docs:  https://orangedatamining.com/
+# PyPi:  https://pypi.org/project/Orange3/
+
+# pip install orange3
+"""
+Starting Orange GUI
+To start Orange GUI from the command line, run:
+
+orange-canvas
+# or
+python3 -m Orange.canvas
+Append --help for a list of program options.
+"""
