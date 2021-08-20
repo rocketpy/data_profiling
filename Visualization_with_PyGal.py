9f4c6f4acb652d9127251dd54aa0831886fb4a93
rocketpy <dimitr.ushakov@gmail.com>
2021-08-20 23:17:12 +0300

    Update Visualization_with_PyGal.py

--- Visualization_with_PyGal.py
+++ Visualization_with_PyGal.py
@@ -1,1 +1,15 @@
+# pip install pygal
+
+
+import pygal
+import seaborn as sns
+
+
+# Loading Dataset
+df = sns.load_dataset('tips')
+
+# Simple Bar Chart
+bar_chart = pygal.Bar()  
+bar_chart.add('Tip', df['tip'])  
+bar_chart.render_to_file('bar_chart.svg')
 
