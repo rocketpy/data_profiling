ea86a41dd902cc4c20c0bdf00a28cd10c4d32177
rocketpy <dimitr.ushakov@gmail.com>
2021-08-21 20:10:19 +0300

    Update Visualization_with_PyGal.py

--- Visualization_with_PyGal.py
+++ Visualization_with_PyGal.py
@@ -9,12 +9,18 @@ df = sns.load_dataset('tips')
 
 # Simple Bar Chart
 bar_chart = pygal.Bar()  
-bar_chart.add('Tip', df['tip'])  
+bar_chart.add('Tips', df['tips'])  
 bar_chart.render_to_file('bar_chart.svg')
 
 
 # Double Bar Chart
 bar_chart.add('Tip', df['tip'][:10])
-bar_chart.add('Total Bill', df['total_bill'][:10])
+bar_chart.add('Total Bill', df['total'][:10])
 bar_chart.render_to_file('bar_chart_2.svg')
+
+
+# Line Chart
+line_chart = pygal.Line()
+line_chart.add('Total', df['total'][:15])
+line_chart.render_to_file('line.svg')
 
