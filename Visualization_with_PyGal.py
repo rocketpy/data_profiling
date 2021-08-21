e4c041b308d82b2783c7a4c6afe5c3ffa0d7fdf0
rocketpy <dimitr.ushakov@gmail.com>
2021-08-21 20:30:16 +0300

    Update Visualization_with_PyGal.py

--- Visualization_with_PyGal.py
+++ Visualization_with_PyGal.py
@@ -29,4 +29,19 @@ line_chart.render_to_file('line.svg')
 line_chart.add('Total', df['total_bill'][:15])
 line_chart.add('Tip', df['tip'][:15])
 line_chart.render_to_file('line_2.svg')
+
+
+# Box Plot
+box_plot = pygal.Box()
+box_plot.title = 'Tips'
+box_plot.add('Tip', df['tip'])
+box_plot.render_to_file('box1.svg')
+
+
+#  Funnel Chart
+funnel_chart = pygal.Funnel()
+funnel_chart.title = 'Total'
+funnel_chart.add('Total', df['total_bill'][:15])
+funnel_chart.add('Tip', df['tip'][:15])
+funnel_chart.render_to_file('funnel.svg')
  
