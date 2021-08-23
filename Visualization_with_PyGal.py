c89d658b5317526a44ed7822b724fb925090fb89
rocketpy <dimitr.ushakov@gmail.com>
2021-08-23 18:08:26 +0300

    Update Visualization_with_PyGal.py

--- Visualization_with_PyGal.py
+++ Visualization_with_PyGal.py
@@ -116,3 +116,14 @@ worldmap_chart.add('In 2012', {
 })
 worldmap_chart.render()
   
+# access to continents
+supra = pygal.maps.world.SupranationalWorld()
+supra.add('Asia', [('asia', 1)])
+supra.add('Europe', [('europe', 1)])
+supra.add('Africa', [('africa', 1)])
+supra.add('North america', [('north_america', 1)])
+supra.add('South america', [('south_america', 1)])
+supra.add('Oceania', [('oceania', 1)])
+supra.add('Antartica', [('antartica', 1)])
+supra.render()
+  
