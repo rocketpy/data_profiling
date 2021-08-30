fe009d78e0287ab4b35e37da4b9085d479eb68ba
rocketpy <dimitr.ushakov@gmail.com>
2021-08-30 22:43:52 +0300

    Update Interactive_visualization.py

--- Interactive_visualization.py
+++ Interactive_visualization.py
@@ -1,1 +1,8 @@
+# Package provides a simple widget-based framework for interactive visualization of algorithms.
+
+# PyPi: https://pypi.org/project/interactive-visualization/
+# Github: https://github.com/Malkovsky/interactive-visualization
+
+# pip install interactive-visualization
+
 
