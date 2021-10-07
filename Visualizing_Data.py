1ad5d627b617a378a9a7ea114fb1ad5f3b7c132f
rocketpy <dimitr.ushakov@gmail.com>
2021-10-07 20:58:01 +0300

    Update Visualizing_Data.py

--- Visualizing_Data.py
+++ Visualizing_Data.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+import requests
+
+
+# get a data
+df_stream_0 = pd.read_json('data_1.json')
+df_stream_1 = pd.read_json('data_2.json')
+
+# merge dataframes
+df_stream = pd.concat([df_stream_0, df_stream_1])
+
+# create a unique ID
+df_stream['UniqueID'] = df_stream['Name'] + ":" + df_stream['Surname']
+# df_stream.head()
 
