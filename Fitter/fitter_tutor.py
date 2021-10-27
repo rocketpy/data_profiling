e0ec7a936f8fe6d95a839504b9c628f1a82f0ce2
rocketpy <dimitr.ushakov@gmail.com>
2021-10-27 21:48:46 +0300

    Update fitter_tutor.py

--- Fitter/fitter_tutor.py
+++ Fitter/fitter_tutor.py
@@ -1,1 +1,16 @@
+# Fitter - A tool to fit data to many distributions and best one(s)
+
+# PyPi: https://pypi.org/project/fitter/
+# Github: https://github.com/cokelaer/fitter
+# Docs: https://fitter.readthedocs.io/en/latest/
+
+# Installation: pip install fitter
+
+# Compatible with Python 3.6, 3.7, and 3.8, 3.9
+
+# A standalone application (very simple) is also provided and works with input CSV files,
+# It creates a file called fitter.png and a log fitter.log
+# fitter fitdist data.csv --column-number 1 --distributions gamma,normal
+
+
 
