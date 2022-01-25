1c1dcf4e36fa7b123383ed63b696d7fd78ddcde3
rocketpy <dimitr.ushakov@gmail.com>
2022-01-25 23:45:20 +0200

    Update joypy.py

--- Visualization/joypy.py
+++ Visualization/joypy.py
@@ -1,1 +1,14 @@
+#  JoyPy is a one-function Python package based on matplotlib + pandas with a single purpose: drawing joyplots (a.k.a. ridgeline plots).
+
+# PyPi: https://pypi.org/project/joypy/0.2.6/
+# Github: https://github.com/leotac/joypy
+
+# pip install joypy==0.2.6
+
+
+import joypy
+import pandas as pd
+
+iris = pd.read_csv("data/iris.csv")
+fig, axes = joypy.joyplot(iris)
 
