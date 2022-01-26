a81b4f1ddc7e85d428893b536233d5fd5a56be77
rocketpy <dimitr.ushakov@gmail.com>
2022-01-26 23:47:14 +0200

    Update joypy.py

--- Visualization/joypy.py
+++ Visualization/joypy.py
@@ -4,7 +4,25 @@
 # Github: https://github.com/leotac/joypy
 
 # pip install joypy==0.2.6
+# pip install joypy
 
+# To install from github, run:
+"""
+git clone git@github.com:leotac/joypy.git
+cd joypy
+pip install .
+"""
+
+# IMPORTANT
+"""
+Dependencies:
+    Python 3.5+
+    Compatibility with python 2.7 has been dropped with release 0.2.0.
+    numpy
+    scipy >= 0.11
+    matplotlib
+    pandas >= 0.20 Warning: compatibility with pandas >= 0.25 requires joypy >= 0.2.1
+"""
 
 import joypy
 import pandas as pd
