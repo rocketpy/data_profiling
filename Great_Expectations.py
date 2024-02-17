72bcfcd55f4f5db5dc34fcd5313f4113ad061e08
rocketpy <dimitr.ushakov@gmail.com>
2024-02-17 23:43:34 +0200

    Create Great_Expectations.py

---
+++ Great_Expectations.py
@@ -0,0 +1,13 @@
+# Great Expectations - Always know what to expect from your data.
+
+# https://github.com/great-expectations/great_expectations
+
+# Quick start
+"""
+pip install great_expectations
+and then run
+
+import great_expectations as gx
+
+context = gx.get_context()
+"""
